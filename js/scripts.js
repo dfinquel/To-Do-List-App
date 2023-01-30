6b77cf859d8820f077b4e47f425ec40ba8e2795b
dfinquel <119966119+dfinquel@users.noreply.github.com>
2023-01-30 22:55:02 +0100

    Add organizational comments within newItem function

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,14 @@
 
 function newItem() {
-    
+
+//Adding a new item to the list of items:
+
+//Crossing out an item from the list of items:
+
+//Adding the delete button "X":
+
+//Adding CLASS DELETE (DISPLAY: NONE) from the css:
+
+//Reordering the items:
+
 }
